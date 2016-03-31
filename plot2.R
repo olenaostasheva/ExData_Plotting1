94638577f1d2d7687627135b59f90b8c0d21ccbb
Olena Ostasheva <oostasheva@middlebury.edu>
2016-03-31 17:18:01 -0400

    Final changes

---
+++ plot2.R
@@ -0,0 +1,19 @@
+# set working directory
+setwd('~/Desktop')
+
+# make sure the plots folder exists
+if (!file.exists('plots')) {
+        dir.create('plots')
+}
+
+# load data
+source('scripts/get_and_clean_data.R')
+
+# open device and set the parameters
+png(filename='plots/plot2.png',width=480,height=480,units='px')
+
+# plot data
+plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
+
+# close device
+x<-dev.off()
